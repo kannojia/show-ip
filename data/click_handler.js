33a972cb3422e4fda0595d190416fa75e04051ff
kannojia <abhishekkannojia.manit@gmail.com>
2014-03-25 11:22:40 +0530

    Added request API to make XMLHttp Request

--- data/click_handler.js
+++ data/click_handler.js
@@ -1,40 +1,17 @@
 
-function getDomainInfo(url) {
-	var a = document.createElement('a');
-	a.href = url;
+function getDomain(url) {
+    var a = document.createElement('a');
+    a.href = url;
 
-	var domain = a.hostname;
-	var requestURL = "http://dazzlepod.com/ip/"+domain+".json";
-    alert(requestURL);
-    console.log(requestURL);
-	return httpGet(requestURL);
+    var domain = a.hostname;
+
+    return domain;
 }
 
-function httpGet(theUrl)
-{
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            console.log(xmlhttp.responseText);
-            return xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
-}
+var check = self.port.on("getDomain", function(url) {
+    var domain = getDomain(url);
 
-var check = self.port.on("clicked", function(url) {
-   	var json = getDomainInfo(url);
-    alert(json);
-    console.log(json);
+    self.port.emit("setDomain",domain);
+
 });
 
